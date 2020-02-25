e9af6d38ee0e04504d97e5f64dc6e8c378ae4575
taniadr <taniadiasr@gmail.com>
2020-02-24 22:42:25 -0300

    day6.py passed

--- Day6.py
+++ Day6.py
@@ -1,28 +1,23 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
-import re
-
 def quebra(wordsSet):
-
     for x in wordsSet :
         auxLine = x
         even = ""
         odd = ""
+        #print ('Case: ', x)
         for i in range (0,len(auxLine)) :
             if i%2 == 0:
                 even += auxLine[i]
             else:
                 odd += auxLine[i]
-        
-        
-        print(even, ' ', odd, "\n")
-
+        print(even, odd)
 
 arrayList = []
 readNLines = int(input())
 while readNLines > 0 :
     line = input()
-    arrayList.append(line)
+    if (line):
+        arrayList.append(line)
     readNLines -= 1
-
-quebra(arrayList[1:])
+quebra(arrayList[:])
 
