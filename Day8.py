5021e4c2f20acc1ead8ace49e3f8c093d77d0055
taniadr <taniadiasr@gmail.com>
2020-02-28 08:43:38 -0300

    Day9.py - recursion in factorial

--- Day8.py
+++ Day8.py
@@ -8,25 +8,18 @@ def search(queries, phoneBook) :
     for x in queries :
         if x not in phoneBook :
             print("Not found")
-
         else :
             print(f"{x}={phoneBook[x]}")
-            #result = x + "=" + phoneBook[x]
-        
-        #print(result)
-
 #Main
-
 n = int(input())
 
 for _ in range(0,n) :
     str = input()       #name
     str = re.split(r'[\s]\s*', str)
     phoneBook[str[0]] = int(str[1])
-#print(phoneBook)
 
 str.clear()
-
+#The number of Queries aren't necessary equals to N
 while True:
     try:
         str = input()
